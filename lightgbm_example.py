f0947a47bcbd2f279449c9ec23132be9fdbf62d0
jehyun <ggurggur123@naver.com>
2023-10-19 11:13:27 +0900

    lightgbm_example.py

--- lightgbm_example.py
+++ lightgbm_example.py
@@ -1,3 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import lightgbm as lgb
+
+df = pd.read_csv('student-por.csv', sep=';') #sep=';' 정렬된 데이터는 생략 가능
+df.head()
+
 y = train.iloc[:, -1]
 X = train.iloc[:, :-1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
